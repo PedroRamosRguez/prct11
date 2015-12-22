abbf35ea297b45ea8b08d756e938b9f0195015ab
alu0100505078 <alu0100505078@ull.edu.es>
2015-12-22 18:41:40 +0000

    Añadida expectativa de libro mediante bloque(modificando constructores)

--- lib/refbiblio_alu0100505078/libro.rb
+++ lib/refbiblio_alu0100505078/libro.rb
@@ -2,6 +2,7 @@
 class Libro < Referencia
 
  #attr_reader  :isbn,:serie, :editorial, :num_edicion
+=begin
    def initialize(autor,isbn,titulo,fecha,editorial,edicion)
 		super(autor,titulo,fecha)
       #variables de instancia
@@ -13,5 +14,16 @@ class Libro < Referencia
     def set_serie(serie)
    	 	@serie=serie
    	 
+    end
+=end 
+ 
+#El constructor recibe un bloque y llama a los metodos que se encuentran en Referencia.
+    def initialize(nombre={},&block)
+        #super(autor,titulo,tipo_doc,nombre)
+        self.nombre=nombre
+        self.tipo_doc=[]
+        self.autor=[]
+        self.titulo=[]
+        instance_eval &block 
     end
 end
--- lib/refbiblio_alu0100505078/referencia.rb
+++ lib/refbiblio_alu0100505078/referencia.rb
@@ -1,7 +1,7 @@
 class Referencia
     include Comparable
     attr_accessor :tipo_doc,:autor,:titulo,:nombre
-
+=begin
     attr_reader :autor,:titulo,:fecha
     def initialize(autor,titulo,fecha)
         @autor=autor
@@ -12,5 +12,69 @@ class Referencia
         t_comparable=self.autor <=> other.autor
         t_comparable
     end
-
+=end
+#constructor recibiendo un bloque del  elemento bibliografico.
+    def initialize(nombre,&block)
+        self.nombre=nombre
+        self.tipo_doc=[]
+        self.autor=[]
+        self.titulo=[]
+        if block_given?  
+            if block.arity == 1
+                yield self
+            else
+                instance_eval &block 
+            end
+        end
+    end
+    
+    def <=>(other)
+        t_comparable=self.autor <=> other.autor
+        t_comparable
+    end
+     #metodo to_s para mostrar en forma de texto
+    def to_s
+        output1 = nombre
+        output1 << "\n#{'=' * nombre.size}\n"
+        output1 << " Autor:\n"
+        
+        autor.reverse.each_with_index do |t_aut, index|
+            output1 << " #{t_aut}\n"
+        end
+        output1 << " #{self.class.to_s}\n"
+        tipo_doc.each_with_index do |t_tipodoc, index|
+            output1 << " #{t_tipodoc}\n"
+        end
+        output1 << " Titulo:"
+        titulo.each_with_index do |t_titulo, index|
+            output1 << " #{t_titulo.capitalize}\n"
+        end
+        output1
+        
+    end
+    
+    
+    #muestra la informacion del autor mediante los hash recibidos.
+    def aut(options ={})
+        t_autor=""
+        t_autor << "\t Apellidos: #{options[:apellido]}" if options[:apellido]
+        t_autor << "\t Nombre: #{options[:nombre]}" if options[:nombre]
+        autor << t_autor
+    end
+    #muestra el tipo de documento mediante los hash recibidos
+    def tipodoc(options={})
+      t_tipodoc=""
+      t_tipodoc << "\t Nombre libro: #{options[:nombre]}" if options[:nombre]
+      t_tipodoc << "\t Volumen: #{options[:volumen]}" if options[:volumen]
+      t_tipodoc << "\t Edicion:#{options[:edicion]}" if options[:edicion]
+      tipo_doc<< t_tipodoc
+      #end
+    end
+  #muestra la informacion del titulo mediante los hash recibidos.
+  def tit(options={})
+      t_titulo=""
+      t_titulo << "#{options[:titulo]}" if options[:titulo]
+      titulo << t_titulo
+  end
+    
 end
